0944842f44cef6bb65e586e42cec5609cad1f44d
Serj <seergey_rti@mail.ru>
2019-10-23 10:29:53 +0300

    Add resize-split directive. Add example how use this directive.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NgxSplitItemComponent } from './ngx-split-item/ngx-split-item.component';
+import { ResizableDirective } from './ngx-split-item/resizable.directive';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NgxSplitItemComponent,
+    ResizableDirective
   ],
   imports: [
     BrowserModule
